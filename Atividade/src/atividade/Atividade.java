b9d729dcdd8b827bf9ecd468969ff31940ab3c1b
Apollon-S <matheusvaristelom@gmail.com>
2024-04-04 09:53:10 -0300

    Adicionando o resto do cardápio e finalizando métodos

--- Atividade/src/atividade/Atividade.java
+++ Atividade/src/atividade/Atividade.java
@@ -17,12 +17,12 @@ public class Atividade {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Restaurante restaurante = new Restaurante("Apollo Yamazaki", "Número do Item: 1 | Nome do prato: Yakisoba de camarão com lula | Preço: 39.90R$\nNúmero do Item: 2 | Nome do prato: Yakisoba de carne | Preço: 32.90R$\nNúmero do Item: 3 | Nome do prato: Yakisoba de frango | Preço: 36.70R$\nNúmero do Item: 4 | Nome do prato: Gyoza (Porção de 12 unidades) | Preço: 10.50R$\nNúmero do Item: 5 | Nome do prato: Temaki de camarão | Preço: 17.70R$\nNúmero do Item: 6 | Nome do Item: Temaki de Salmão | Preço: 14.90R$\nNúmero do Item: 7 | Nome do Item: Missoshiru | Preço: 7.99R$\nNúmero do item: 8 | Nome do Item: Teppan de salmão | Preço: 26.50R$\nNúmero do item: 9 | Nome do Item: Lámen | Preço: 44.90R$\nNúmero do item: 10 | Nome do Item: Sushi 40 peças | Preço: 109.90R$");
-        Caixa caixa01 = new Caixa("Matheus", 6, "+55 (41) 11111-1111", "109.876.543-21", 1859, "Vale refeição, vale transporte e GymPass", "Feriados nacionais", "Das 8 às 18h");
-        caixa01.mostrarinfo();
-        Cliente clienteteste = caixa01.cadastrodocliente();
-        clienteteste.mostrarinfo();
-
+        Restaurante restaurante = new Restaurante("Apollo Yamazaki", "Número do Item: 1 | Nome do prato: Yakisoba de camarão com lula | Preço: 39.90R$\nNúmero do Item: 2 | Nome do prato: Yakisoba de carne | Preço: 32.90R$\nNúmero do Item: 3 | Nome do prato: Yakisoba de frango | Preço: 36.70R$\nNúmero do Item: 4 | Nome do prato: Gyoza (Porção de 12 unidades) | Preço: 10.50R$\nNúmero do Item: 5 | Nome do prato: Temaki de camarão | Preço: 17.70R$\nNúmero do Item: 6 | Nome do Item: Temaki de Salmão | Preço: 14.90R$\nNúmero do Item: 7 | Nome do Item: Missoshiru | Preço: 7.99R$\nNúmero do item: 8 | Nome do Item: Teppan de salmão | Preço: 26.50R$\nNúmero do item: 9 | Nome do Item: Lámen | Preço: 44.90R$\nNúmero do item: 10 | Nome do Item: Sushi 40 peças | Preço: 109.90R$\nNúmero do Item: 11 | Nome da bebida: Água Mineral (500ml) | Preco: 3.50R$\nNúmero do Item: 12 | Nome da bebida: Refrigerante (Lata) | Preco: 5.00R$\nNúmero do Item: 13 | Nome da bebida: Suco Natural (250ml) | Preco: 7.50R$\nNúmero do Item: 14 | Nome da bebida: Chá Gelado (300ml) | Preco: 4.50R$\nNúmero do Item: 15 | Nome da bebida: Café Expresso | Preco: 4.00R$");
+        Caixa Arthur = new Caixa("Arthur Veiga", 16, "(41) 99999-9999", "111.111.111-11", 1894, "Vale alimentação, refeição, transporte e Gympass", "Todo feriado nacional", "Das 8h às 18h");
+        Garçom Luiz = new Garçom("Luiz Brum", 16, "(41) 11111-1111", "222.222.222-22", 1474, "Vale alimentação, refeição e transporte", "Todo feriado nacional e todo dia 5 do mês", "Das 8h às 22h" );
+        Supervisor Matheus = new Supervisor("Matheus Varistelo", 16, "(41) 55555-5555", "555.555.555-55", 8977, "Vale alimentação, refeição, transporte, acesso à plano de saúde e psicológico, cursos gratuitos no sistema 'Apollo', Gympass e cartão de desconto na compra de automóveis da linha Toyota", "Todo feriado nacional, 4 dias ao mês de escolha do funcionário", "Das 11h às 19h");
+        Arthur.cadastrodocliente();
+        Luiz.mostrarcardápio();
     }
 
 }
@@ -67,7 +67,7 @@ class Cliente extends Pessoa {
     }
 
     public void fazerpedido() {
-        while (escolhadocliente != 0 && escolhadocliente < 11) {
+        while (escolhadocliente != 0) {
             escolhadocliente = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do Item do Cliente: "));
             quantidadeescolhadocliente = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de unidades do item que o cliente quer: "));
             switch (escolhadocliente) {
@@ -101,10 +101,27 @@ class Cliente extends Pessoa {
                 case 10:
                     gastos = gastos + (109.90 * quantidadeescolhadocliente);
                     break;
+                    case 11:
+                    gastos = gastos + (3.50 * quantidadeescolhadocliente); 
+                    break;
+                case 12:
+                    gastos = gastos + (5.00 * quantidadeescolhadocliente); 
+                    break;
+                case 13:
+                    gastos = gastos + (7.50 * quantidadeescolhadocliente); 
+                case 14:
+                    gastos = gastos + (4.50 * quantidadeescolhadocliente); 
+                    break;
+                case 15:
+                    gastos = gastos + (4.00 * quantidadeescolhadocliente); 
+                default:
+                    JOptionPane.showMessageDialog(null, "Número do Item inválido.");
+                    break;
+            }
             }
         }
 
-    }
+    
 
     public Cliente(String nome, int idade, String telefone, String cpf) {
         super(nome, idade, telefone, cpf);
@@ -125,10 +142,10 @@ class Funcionário extends Pessoa {
 
 class Supervisor extends Funcionário {
     double alterarcontadocliente(Cliente cliente) {
-        cliente.gastos = 
+        cliente.gastos = JOptionPane.showInputDialog(Double.parseDouble("Olá, senhor supervisor! Digite o número no qual se deve alterar o valor da conta do cliente: "));
     }
     double vercontadoclientesupervisor(Cliente cliente) {
-        System.out.println(cliente.gastos);
+        JOptionPane.showMessageDialog(null, cliente.gastos);
         return cliente.gastos;
     }
     public Supervisor(String nome, int idade, String telefone, String cpf, float salário, String benefícios, String Folga, String horáriodetrabalho) {
@@ -138,7 +155,7 @@ class Supervisor extends Funcionário {
 
 class Garçom extends Funcionário {
 
-    public String cardápiodogarçom = "Número do Item: 1 | Nome do prato: Yakisoba de camarão com lula | Preço: 39.90R$\nNúmero do Item: 2 | Nome do prato: Yakisoba de carne | Preço: 32.90R$\nNúmero do Item: 3 | Nome do prato: Yakisoba de frango | Preço: 36.70R$\nNúmero do Item: 4 | Nome do prato: Gyoza (Porção de 12 unidades) | Preço: 10.50R$\nNúmero do Item: 5 | Nome do prato: Temaki de camarão | Preço: 17.70R$\nNúmero do Item: 6 | Nome do Item: Temaki de Salmão | Preço: 14.90R$\nNúmero do Item: 7 | Nome do Item: Missoshiru | Preço: 7.99R$\nNúmero do item: 8 | Nome do Item: Teppan de salmão | Preço: 26.50R$\nNúmero do item: 9 | Nome do Item: Lámen | Preço: 44.90R$\nNúmero do item: 10 | Nome do Item: Sushi 40 peças | Preço: 109.90R$";
+    public String cardápiodogarçom = "Número do Item: 1 | Nome do prato: Yakisoba de camarão com lula | Preço: 39.90R$\nNúmero do Item: 2 | Nome do prato: Yakisoba de carne | Preço: 32.90R$\nNúmero do Item: 3 | Nome do prato: Yakisoba de frango | Preço: 36.70R$\nNúmero do Item: 4 | Nome do prato: Gyoza (Porção de 12 unidades) | Preço: 10.50R$\nNúmero do Item: 5 | Nome do prato: Temaki de camarão | Preço: 17.70R$\nNúmero do Item: 6 | Nome do Item: Temaki de Salmão | Preço: 14.90R$\nNúmero do Item: 7 | Nome do Item: Missoshiru | Preço: 7.99R$\nNúmero do item: 8 | Nome do Item: Teppan de salmão | Preço: 26.50R$\nNúmero do item: 9 | Nome do Item: Lámen | Preço: 44.90R$\nNúmero do item: 10 | Nome do Item: Sushi 40 peças | Preço: 109.90R$\nNúmero do Item: 11 | Nome da bebida: Água Mineral (500ml) | Preco: 3.50R$\nNúmero do Item: 12 | Nome da bebida: Refrigerante (Lata) | Preco: 5.00R$\nNúmero do Item: 13 | Nome da bebida: Suco Natural (250ml) | Preco: 7.50R$\nNúmero do Item: 14 | Nome da bebida: Chá Gelado (300ml) | Preco: 4.50R$\nNúmero do Item: 15 | Nome da bebida: Café Expresso | Preco: 4.00R$";
 
     public void mostrarcardápio() {
         JOptionPane.showMessageDialog(null, cardápiodogarçom);
